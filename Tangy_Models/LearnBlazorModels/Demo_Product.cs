9d3cb48213322571b854ecf02374865d05b253e9
Ryan H <8751750+rynhndrcksn@users.noreply.github.com>
2022-04-07 23:15:18 -0700

    Section 16 done.
    Added Tangy_Models with a Demo_Product model. Bound that model to BindProp.razor

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Tangy_Models.LearnBlazorModels
+{
+	public class Demo_Product
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+
+
+	}
+}
+
