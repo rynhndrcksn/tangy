bc341006713a0dee590f0c177966d50b3f66203b
Ryan H <8751750+rynhndrcksn@users.noreply.github.com>
2022-04-28 22:09:31 -0700

    Section 20 done.
    Added extra property that hold more properties.

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -7,7 +7,7 @@ namespace Tangy_Models.LearnBlazorModels
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
-
+		public IEnumerable<Demo_ProductProp> ProductProps { get; set; }
 
 	}
 }
