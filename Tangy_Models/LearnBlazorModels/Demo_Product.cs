e0627287179acb9111313a9d7df7315be64ed585
Ryan H <8751750+rynhndrcksn@users.noreply.github.com>
2022-04-28 20:39:17 -0700

    Section 17 done.
    Added single and two way data binding.

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -5,6 +5,7 @@ namespace Tangy_Models.LearnBlazorModels
 	{
         public int Id { get; set; }
         public string Name { get; set; }
+        public double Price { get; set; }
         public bool IsActive { get; set; }
 
 
