61a4a6a2f9d4df38bec3c7c7f238c1da753b872e
Ryan H <8751750+rynhndrcksn@users.noreply.github.com>
2022-05-14 18:59:19 -0700

    Sect. 6, step 50 done.
    Added JQuery/Toastr to BlazorJS and got it working. Unable to refactor atm.

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful");
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed");
+    }
+}
